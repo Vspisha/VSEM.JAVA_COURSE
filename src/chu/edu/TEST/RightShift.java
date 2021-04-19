bd9b37f50731f1247b27f46c10b51ec3ffe147e7
Vspisha <Vspisha321@gmail.com>
2021-04-19 20:57:32 +0300

    FINAL TEST

--- src/chu/edu/TEST/RightShift.java
+++ src/chu/edu/TEST/RightShift.java
@@ -11,25 +11,24 @@ import java.util.Arrays;
  * @since 19.04.2021 - 17.59
  **/
 public class RightShift {
-        public static int[] rightShift(int[] array, int step){
-            int[] Array = new int[array.length];
-            int Index = 0;
-            int Step = 0;
-            if (step >= array.length){
-                Step = step % array.length;
-            } else {
-                Step = step;
-            }
-
-            for (int i = 0; i < array.length; i++) {
-                Index = i+Step;
-                if (i+Step >= array.length){
-                    Index = i+Step - array.length;
-                }
-                Array[Index] = array[i];
-            }
-            return Array;
+    public static int[] rightShift(int[] array, int step) {
+        int[] Array = new int[array.length];
+        int Index = 0;
+        int Step = 0;
+        if (step >= array.length) {
+            Step = step % array.length;
+        } else {
+            Step = step;
         }
+        for (int i = 0; i < array.length; i++) {
+            Index = i + Step;
+            if (i + Step >= array.length) {
+                Index = i + Step - array.length;
+            }
+            Array[Index] = array[i];
+        }
+        return Array;
+    }
 
 
     public static void main(String[] args) {
